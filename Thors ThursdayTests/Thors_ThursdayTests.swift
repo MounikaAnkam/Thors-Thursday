1be2708435a1725b79d03304355f78eb74f3fc35
Michael Rogers <mprogers@mbppstarphoenix.nwmissouri.edu>
2015-01-29 09:43:07 -0600

    Initial Commit

---
+++ Thors ThursdayTests/Thors_ThursdayTests.swift
@@ -0,0 +1,36 @@
+//
+//  Thors_ThursdayTests.swift
+//  Thors ThursdayTests
+//
+//  Created by Michael Rogers on 1/29/15.
+//  Copyright (c) 2015 Michael Rogers. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Thors_ThursdayTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
